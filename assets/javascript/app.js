b6ca56b587e50d25ad03af8dde93f16645c0f079
NASA <chrisjwill93@gmail.com>
2018-04-18 12:51:33 -0400

    push

---
+++ assets/javascript/app.js
@@ -0,0 +1,24 @@
+
+    //countdown timer
+    var seconds = 120;
+        function secondsPassed(){
+            var minutes = Math.round((seconds - 30) / 60);
+            var remainingSeconds = seconds % 60;
+
+            if(remainingSeconds < 10){
+                remainingSeconds = "0" + remainingSeconds;
+            }
+            
+            document.getElementById('countdown').innerHTML = minutes + ":" + remainingSeconds
+
+            if(seconds == 0){
+                clearInterval(countdownTimer);
+                document.getElementById('countdown').innerHTML = "Game Over";
+            }else {
+                seconds --;
+            }
+        }
+        var countdownTimer = setInterval('secondsPassed()', 1000);
+
+
+
